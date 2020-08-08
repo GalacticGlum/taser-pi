eb9f2da9de19f752824dbf725fd70aa657be10a7
galacticglum <nerftactic09@gmail.com>
2020-08-08 14:46:12 +0000

    Added concurrency locking to relay switch thread

--- app.py
+++ app.py
@@ -7,11 +7,25 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('local_config.py')
 
+# A boolean indicating whether the relay thread is running currently
+# This is so that we don't try to switch the state of the relay simultaneously.
+is_switch_relay_running = False
+lock = threading.Lock()
+
 def switch_relays(time_ms):
+    global is_switch_relay_running
+    # Bail if another thread is running
+    if is_switch_relay_running: return
+    with lock:
+        is_switch_relay_running = True
+
     print('Relay closed')
     # Convert milliseconds to seconds for sleep method
     time.sleep(time_ms / 1000)
     print('Relay open')
+
+    with lock:
+        is_switch_relay_running = False
 
 
 @app.route('/api/shock')
